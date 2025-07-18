5940ac260b98606f862a821aa3f9dde29701f0f0
JP-Pardinho <jppardinhor@gmail.com>
2025-07-18 19:17:09 -0300

    Criando repositorio

---
+++ aulas/ex000/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pi-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Me livrando da procrastinação</title>
+</head>
+
+<body>
+    <h1>
+        <?php
+        echo "Meu primeiro site usando php! \u{1F30E}";
+        ?>
+    </h1>
+    <p>Vamos tentar se livrar da procrastinação</p>
+</body>
+
+</html>
